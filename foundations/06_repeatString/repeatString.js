03aaaeb752237d026bcfc36af0037da670ded57c
thanhtd <thanhtd@sabitech.vn>
2026-03-16 16:28:03 +0700

    Finish 06 foundation js

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let output = "";
+  for (let i = number; i > 0; i--) {
+    if (i < 0) {
+      return "ERROR";
+    }
+    output += string;
+  }
+  return output;
 };
 
 // Do not edit below this line
