3583ba9449beda950392ca13dc723aade44ca9c0
thanhtd <thanhtd@sabitech.vn>
2026-03-17 09:51:35 +0700

    Finish 09 foundation-js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, secondNumber) {
+  let sum = 0;
+  if (
+    firstNumber < 0 ||
+    secondNumber < 0 ||
+    !Number.isInteger(firstNumber) ||
+    !Number.isInteger(secondNumber) ||
+    typeof firstNumber !== "number" ||
+    typeof secondNumber !== "number"
+  ) {
+    return "ERROR";
+  }
+  if (firstNumber > secondNumber) {
+    for (let i = secondNumber; i <= firstNumber; i++) {
+      sum += i;
+    }
+  }
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
