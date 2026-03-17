379f5a887820b3951ee3c7b64265124886b0c182
thanhtd <thanhtd@sabitech.vn>
2026-03-17 08:16:22 +0700

    Finish 07 foundation-js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let output = "";
+  const arr = string.split("");
+  for (let i = arr.length - 1; i >= 0; i--) {
+    output += arr[i];
+  }
+  return output;
 };
 
 // Do not edit below this line
