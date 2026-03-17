e5f832aa6e0c2803a17030b9ace8d3961649bab3
thanhtd <thanhtd@sabitech.vn>
2026-03-17 09:11:30 +0700

    Finish 08 foundation-js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemToRemove) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    for (const item of itemToRemove) {
+      if (item === array[i]) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
