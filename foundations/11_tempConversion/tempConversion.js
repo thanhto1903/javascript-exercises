c1dc0ea225b66ab5741a52a5335bde696b0a6772
thanhtd <thanhtd@sabitech.vn>
2026-03-17 10:22:23 +0700

    Finish 11 foundation-js

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  const fahrenheit = (degree - 32) * (5 / 9);
+  const rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  const celcius = (degree * 9) / 5 + 32;
+  const rounded = Math.round(celcius * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
