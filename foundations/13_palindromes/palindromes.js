109e6337a67c2d7e2857b7516a87848b3e6b28ca
thanhtd <thanhtd@sabitech.vn>
2026-04-20 16:08:30 +0700

    finish 14

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const clean = string.toLowerCase().replace(/[," ".!]/g, "");
+  const reversed = clean.split("").reverse().join("");
+  if (clean === reversed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
