ef863adbbb3e98fd031b9b310f417542d9a28420
thanhtd <thanhtd@sabitech.vn>
2026-04-20 17:21:06 +0700

    finish 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) {
+    return "OOPS";
+  }
+  let arr = [0, 1];
+  for (let i = 0; i < number; i++) {
+    arr.push(arr[i] + arr[i + 1]);
+    console.log(arr);
+  }
+  return arr[number];
 };
 
 // Do not edit below this line
